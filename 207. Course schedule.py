619917d628803985e9f4f21fd445bb29e6447d74
Aruthra Sathish Kumar <126383547+Aruthrasathishkumar@users.noreply.github.com>
2026-04-14 23:51:46 -0400

    Create 207. Course schedule.py

---
+++ 207. Course schedule.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
+
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        completed = 0
+
+        while queue:
+            course = queue.popleft()
+            completed += 1
+
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return completed == numCourses
