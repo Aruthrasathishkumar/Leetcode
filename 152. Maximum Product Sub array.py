067fb249f1a47086c1e87a4375cb4d89af5d95c5
Aruthra Sathish Kumar <126383547+Aruthrasathishkumar@users.noreply.github.com>
2026-06-02 22:21:12 -0400

    Create 152. Maximum Product Sub array.py

---
+++ 152. Maximum Product Sub array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        CurMax, CurMin = 1, 1
+
+        for n in nums:
+            temp = CurMax
+            
+            CurMax = max(n * CurMax, n * CurMin, n)
+            CurMin = min(n * temp, n * CurMin, n)
+            res = max(res, CurMax)
+        return res
