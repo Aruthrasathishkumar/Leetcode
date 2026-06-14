ee367a76f26e2ddd48b24cbb63a3def8ae3147b8
Aruthra Sathish Kumar <126383547+Aruthrasathishkumar@users.noreply.github.com>
2026-06-13 23:03:57 -0400

    Update 1339. Maximum Product of splitted binary tree.py

--- 1339. Maximum Product of splitted binary tree.py
+++ 1339. Maximum Product of splitted binary tree.py
@@ -1,3 +1,4 @@
+# Leetcode 1339
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -21,7 +22,7 @@ class Solution:
             subtree_sums.append(total)
 
             return total
-
+ 
         total_sum = dfs(root)
 
         max_product = 0
